c38fc327a358f063efc8d9ed40a0668bebc6f028
A-Game <javascript@adamheldring.com>
2018-10-04 15:49:55 +0200

    Search with filter for Riks/Lokal

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -3,21 +3,23 @@ import Station from "./Station/station.js"
 
 class App extends React.Component {
 state = {
-  radioList: []
+  radioList: [],
+  filteredRadioList: [],
+  userSearchString: ""
 }
+
 
 componentDidMount = () => {
   const url = "http://api.sr.se/api/v2/channels?format=json&size=100"
   fetch(url)
     .then(response => {
-      console.log(response)
       return response.json()
     })
     .then(result => {
-      console.log(result)
       console.log(result.channels)
       this.setState({
-        radioList: result.channels
+        radioList: result.channels,
+        filteredRadioList: result.channels
       })
     })
 }
@@ -25,17 +27,42 @@ componentDidMount = () => {
 componentWillUnmount = () => {
 }
 
+filterList = () => {
+  this.setState({
+    filteredRadioList: this.state.radioList.filter(channel => {
+      return channel.channeltype.indexOf(this.state.userSearchString) != -1})
+    })
+}
+
+
+userSearch = (e) => {
+  this.setState({
+    userSearchString: e.target.value
+  })
+  this.filterList()
+}
+
 render() {
+
   if (this.state.radioList.length > 0) {
     return (
       <div className="master-wrapper">
+
         <header>
           <h1>RADIO</h1>
         </header>
-        <section className="stations-container">
-          <h3 className="nrStations-listed">Number of stations listed: {this.state.radioList.length}</h3>
 
-          {this.state.radioList.map((channel, index) => {
+        <section className="search-container">
+          <h3 className="nrStations-listed">Antal listade kanaler: {this.state.radioList.length}</h3>
+          <div className="searchContainer">
+            <input type="text" className="searchBar" name="radioSearch" placeholder="Sök efter nyckelord" onChange={this.userSearch}/>
+          </div>
+        </section>
+
+        <section className="stations-container">
+          {console.log(this.state.userSearchString)}
+          {console.log(this.state.filteredRadioList)}
+          {this.state.filteredRadioList.map((channel, index) => {
             return <Station
               key={index}
               id={channel.id}
